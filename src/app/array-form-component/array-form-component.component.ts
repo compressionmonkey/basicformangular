94c6bb51bbb741d65f04c3cb127200ce30af0e05
keldendraduldorji <kelden.dorji@6dtech.co.in>
2021-02-08 12:29:49 +0530

    tested of and map operator

--- src/app/array-form-component/array-form-component.component.ts
+++ src/app/array-form-component/array-form-component.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
-import { fromEvent } from 'rxjs';
+import { fromEvent, observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
 @Component({
   selector: 'app-array-form-component',
   templateUrl: './array-form-component.component.html',
@@ -11,10 +13,23 @@ export class ArrayFormComponentComponent implements OnInit {
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    const nums = of(1,2,3);
+    const squareValues = map((val: number) => val * val);
+    const squaredNums = squareValues(nums);
+
+    squaredNums.subscribe(x => console.log("poooo",x));
+
     this.myForm = this.fb.group({
       email: '',
       phones: this.fb.array([])
     });
+    // ajax observable
+    const apiData = ajax('https://jsonplaceholder.typicode.com/todos/1');
+    apiData.subscribe(
+      res => console.log('api is',res.status, res.response)
+    )
+    // from event observable
     const el = document.getElementsByClassName('formPos');
     const mouseMoves = fromEvent(el, 'mousemove');
     const subscription = mouseMoves.subscribe(
